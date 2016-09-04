7effd55be5354516776983f43d0162f6207eaacc
root <root@localhost.localdomain>
2016-09-04 01:41:09 -0400

    updated textgame

--- textGame/src/nycc4q/Main.java
+++ textGame/src/nycc4q/Main.java
@@ -86,25 +86,47 @@ public class Main {
     public static void enterGame(){
         int enterVal;
         do {
-            System.out.println("(1) Start New Game  |   (2) Quit Game  | ");
+            System.out.println("                                     (1) Start New Game  |   (2) Quit Game  | ");
             System.out.println("Enter Value: ");
             enterVal = Scan();
-
-            System.out.println(enterVal); // test enterVal
+           // System.out.println(enterVal); // test enterVal
 
         }while(enterVal != 1 && enterVal != 2); // "==" would compare int !=  --> comparing memory address... vs .equals is comparing primitives.
+        printDelay(20, "Loading % ****************************************************************************************************************************");
 
         return;
     }
 
     public static void startStory(){
+        System.out.println("\n \n ");
         printDelay(2,"Hello, welcome to Date Me!   |");
         printDelay(2,"What is your name: ______?   | ");
         String name = Read();
         printDelay(2,"What is your favorite food between _______:  |");
-        System.out.println("(1) CheeseBurger | (2) Pizza | (3) Chicken Wings ");
+        System.out.println("(1) CheeseBurger | (2) Pizza | (3) Chicken Wings | (4)  Fish |  (5) Salad ");
         String food = Read();
         printDelay(2,"What is your best feature?   |");
+        String feature = Read();
+        printDelay(2,"*******************************************************************************************************************************");
+        printDelay(2,"*******************************************************************************************************************************");
+        printDelay(2,"*******************************************************************************************************************************");
+        printDelay(2,"*******************************************************************************************************************************");
+        printDelay(2,"*******************************************************************************************************************************");
+        printDelay(2,"                                                               ****************************************************************");
+        printDelay(2,"Hello " + name + " You are in play date with 3 contestants!               **************************************************************************");
+        printDelay(2,"You will ask each contestant one question per round..........  *****");
+        printDelay(2,"At the end of each round, You will eliminate one contestant!   *****");
+        printDelay(2,"                                                               *****");
+        printDelay(2,"                                                               *****");
+        printDelay(2,"                                                               *****");
+        printDelay(2,"          ARE YOU READY TO PLAY DATE ?                         *****");
+        printDelay(2,"");
+        printDelay(2,"");
+        printDelay(2,"");
+        printDelay(2,"");
+        printDelay(2,"");
+        printDelay(2,"");
+
     }
 
 }
